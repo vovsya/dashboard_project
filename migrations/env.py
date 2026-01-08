1cc1b613cf58ffcc8afbf3c2b440ae6cdde22fd6
Vladimir Biriukov <wowas18@icloud.com>
2026-01-08 20:59:01 +0300

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -8,8 +8,8 @@ import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-config = context.config
 db_connection = os.getenv("SYNC_DB_URL")
+config = context.config
 config.set_main_option("sqlalchemy.url", db_connection)
 
 # Interpret the config file for Python logging.
