a63080f30999eefbb835cd6f151a563cad934d3e
Vladimir Biriukov <wowas18@icloud.com>
2026-01-09 10:48:11 +0300

    Update app.py

--- dashboard_app/dashboard/core/app.py
+++ dashboard_app/dashboard/core/app.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 from dashboard_app.dashboard.routes.pages import pages_router
 from dashboard_app.dashboard.routes.profile import profile_router
 from dashboard_app.dashboard.routes.widgets import widgets_router
+from fastapi.responses import RedirectResponse
+
 
 dashboard_app = FastAPI()
 
 dashboard_app.include_router(pages_router)
 dashboard_app.include_router(profile_router)
 dashboard_app.include_router(widgets_router)
+
+@dashboard_app.get("/", tags=["Корень"])
+def root():
+    return RedirectResponse(url="/docs")
